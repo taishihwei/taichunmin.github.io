b50a34a3edf0ef1946c39a9a88d611e9c6c2d264
taichunmin <taichunmin@gmail.com>
2018-04-19 03:00:17 +0800

    程式碼暫存

---
+++ .vuepress/config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack')
+
+module.exports = {
+  title: '筆記國度',
+  description: '目前任職於微程式資訊股份有限公司，從高中因為社團接觸 ACM，然後大學開始自學 PHP。主要是後端工程師，前端略有涉略，美工超級不擅長，請大家多多指教。',
+  evergreen: true,
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: '關於我', link: '/about-me' },
+      { text: '經歷', link: '/experiences' },
+      { text: '技能', link: '/skills' },
+      { text: '作品集', link: '/works' },
+      { text: '關於本站', link: '/about-site' },
+    ],
+    repo: 'taichunmin/blog',
+    editLinks: false
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        '$': 'jquery',
+        'jQuery': 'jquery',
+        'Popper': 'popper.js'
+      })
+    ]
+  }
+}
--- readme.md
+++ readme.md
@@ -1,21 +1,15 @@
-# 筆記國度
-
-Author: 和風信使
-
-## About
-
-此網站使用 [VuePress](https://vuepress.vuejs.org/guide/getting-started.html) 來建置，建置教學如下：
-
-```sh
-# install globally
-yarn global add vuepress # OR npm install -g vuepress
-
-# create a markdown file
-echo '# Hello VuePress' > README.md
-
-# start writing
-vuepress dev
-
-# build
-vuepress build
-```
+---
+# layout: LayoutHome
+home: true
+heroImage: https://i.imgur.com/pLlxV0M.jpg
+heroText: 和風信使
+actionText: 部落格 →
+actionLink: /articles/
+footer: CC BY-NC-SA 3.0 | Copyright © 2014-present Chunmin Tai
+meta:
+  - name: keywords
+    content: taichunmin
+---
+::: warning 建置中
+這個部落格正在建置中，希望能把以前四散在各地的部落格資料都集中放到這裡。
+:::
