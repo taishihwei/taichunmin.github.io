7f297e95b2e2fbd7bf09d73815deecf4594725ab
taichunmin <taichunmin@gmail.com>
2018-04-27 18:43:31 +0800

    於經歷頁面顯示 NProgress

--- .vuepress/components/LayoutExperiences.vue
+++ .vuepress/components/LayoutExperiences.vue
@@ -1,14 +1,23 @@
 <template lang="pug">
-  iframe#iframe-experiences(:src="'https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=' + data.spreadsheets + '&font=OpenSans-GentiumBook&lang=zh-tw&start_at_end=true&initial_zoom=0&hash_bookmark=true'", width="100%", webkitallowfullscreen="", mozallowfullscreen="", allowfullscreen="", frameborder="0")
+  iframe#iframe-experiences(:src="'https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=' + data.spreadsheets + '&font=OpenSans-GentiumBook&lang=zh-tw&start_at_end=true&initial_zoom=0&hash_bookmark=true'", width="100%", webkitallowfullscreen="", mozallowfullscreen="", allowfullscreen="", frameborder="0", @load="loaded")
 </template>
 
 <script>
+import NProgress from 'nprogress'
 export default {
   computed: {
     data () {
       return this.$page.frontmatter
     }
-  }
+  },
+  mounted () {
+    NProgress.start()
+  },
+  methods: {
+    loaded () {
+      NProgress.done()
+    }
+  } 
 }
 </script>
 
