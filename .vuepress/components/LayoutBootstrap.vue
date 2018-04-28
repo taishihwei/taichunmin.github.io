5eba3745fd36ea6d51c86410f7e32bd82f096fde
taichunmin <taichunmin@gmail.com>
2018-04-28 16:14:50 +0800

    小修正

--- .vuepress/components/LayoutBootstrap.vue
+++ .vuepress/components/LayoutBootstrap.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .container
+  .container.layout-bootstrap
     Content
 </template>
 
@@ -16,13 +16,9 @@ export default {
 }
 </script>
 
-
 <style lang="sass">
-@import '~bootstrap/dist/css/bootstrap.css'
-@import '~font-awesome/css/font-awesome.css'
-
 body
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif, "文泉驛正黑", "WenQuanYi Zen Hei", "儷黑 Pro", "LiHei Pro", "微軟正黑體", "Microsoft JhengHei", "標楷體", DFKai-SB, sans-serif
-.container
-  margin-top: -2.6rem
+.layout-bootstrap
+  margin-top: 1rem
 </style>
---
+++ works.md
@@ -0,0 +1,11 @@
+---
+works:
+- title: 中興大學機構典藏
+  desc: <a href="http://tair.org.tw/" target="_blank">臺灣學術機構典藏系統</a>維護與功能新增
+  url: http://nchuir.lib.nchu.edu.tw/
+  album:
+  - 
+---
+# <span class="fa fa-file-code-o fa-fw"></span> 作品集
+
+包含網頁，小工具，程式作業以及其他作品。
