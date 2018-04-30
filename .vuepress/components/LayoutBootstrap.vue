86acb3f053161e8d1deeac36b5b7f7b712d1c761
taichunmin <taichunmin@gmail.com>
2018-05-01 02:29:42 +0800

    嘗試修正字型

--- .vuepress/components/LayoutBootstrap.vue
+++ .vuepress/components/LayoutBootstrap.vue
@@ -18,8 +18,6 @@ export default {
 
 <style src="bootstrap/dist/css/bootstrap.css"></style>
 <style lang="sass">
-body
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif, "文泉驛正黑", "WenQuanYi Zen Hei", "儷黑 Pro", "LiHei Pro", "微軟正黑體", "Microsoft JhengHei", "標楷體", DFKai-SB, sans-serif
 .layout-bootstrap
   margin-top: 1rem
 </style>
