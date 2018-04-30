fd54fb8ea891b618f126db48d37aa3504335dba0
taichunmin <taichunmin@gmail.com>
2018-05-01 02:16:40 +0800

    修正作品集的 modal 和 carousel

--- .vuepress/components/WorkCard.vue
+++ .vuepress/components/WorkCard.vue
@@ -5,53 +5,54 @@ section.work-card
     .card-body
       h5.card-title {{ work.title }}
       p.card-text(v-html="work.desc")
-      a.btn.btn-primary.mr-1(v-if="_.get(work, 'album.length', 0)", data-toggle="modal", :data-target="'#' + cardId") #[span.fa.fa-picture-o] 圖集
+      button.btn.btn-primary.mr-1(v-if="_.get(work, 'album.length', 0)", type="button", data-toggle="modal", :data-target="'#' + modalId") #[span.fa.fa-picture-o] 圖集
       a.btn.btn-primary(v-if="work.url", :href="$withBase(work.url)", target="_blank") #[span.fa.fa-external-link] 網站
-  .modal(tabindex="-1", role="dialog", :id="cardId")
+  .modal.fade(tabindex="-1", role="dialog", :id="modalId", :aria-labelledby="`${modalId}-title`", aria-hidden="true")
     .modal-dialog.modal-dialog-centered.modal-lg(role="document")
       .modal-content
         .modal-header
-          h5.modal-title 作品集：{{ work.title }}
+          h5.modal-title(:id="`${modalId}-title`") 作品集：{{ work.title }}
           button.close(type="button", data-dismiss="modal", aria-label="Close")
             span(aria-hidden="true") &times;
         .modal-body
           .carousel.slide(v-if="_.get(work, 'album.length', 0)", :id="albumId", data-ride="carousel", data-interval="false", data-wrap="false")
-            ol.carousel-indicators(v-if="indicators")
+            ol.carousel-indicators(v-show="caption")
               li(v-for="item, index in work.album", :key="item.img", :data-target="'#' + albumId", :data-slide-to="index", :class="{active: index === 0}")
             .carousel-inner
               .carousel-item(v-for="item, index in work.album", :key="item.img", :class="{active: index === 0}")
                 img.d-block.w-100(:src="$withBase(item.img)", :alt="item.alt || ''")
-                .carousel-caption.rounded
-                  p(v-if="", v-html="item.caption")
-            a.carousel-control-prev(:href="'#' + albumId", role="button", data-slide="prev", v-if="controlPrevNext")
+                .carousel-caption.rounded(v-show="caption")
+                  p(v-html="item.caption")
+            a.carousel-control-prev(:href="'#' + albumId", role="button", data-slide="prev")
               span.carousel-control-prev-icon(aria-hidden="true")
               span.sr-only Previous
-            a.carousel-control-next(:href="'#' + albumId", role="button", data-slide="next", v-if="controlPrevNext")
+            a.carousel-control-next(:href="'#' + albumId", role="button", data-slide="next")
               span.carousel-control-next-icon(aria-hidden="true")
               span.sr-only Next
         .modal-footer
-          button.btn.btn-primary(type="button", data-dismiss="modal") Close
+          button.btn.btn-info(type="button", @click="caption=!caption") 開關圖片說明
+          a.btn.btn-primary(v-if="work.url", :href="$withBase(work.url)", target="_blank") #[span.fa.fa-external-link] 網站
+          button.btn.btn-secondary(type="button", data-dismiss="modal") 關閉
 </template>
 
 <script>
 export default {
   props: ['work'],
   computed: {
-    cardId () {
-      return 'workcard-' + this._uid
+    modalId () {
+      return 'workmodal-' + this._uid
     },
     albumId () {
       return 'workalbum-' + this._uid
     }
   },
   data: () => ({
-    indicators: true,
-    controlPrevNext: true
+    caption: true
   })
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="sass">
   .card-body
     border-top: 1px solid rgba(0, 0, 0, 0.125)
   a:not([href]):not([tabindex])
@@ -61,6 +62,9 @@ export default {
     background-color: rgba(0, 0, 0, 0.6)
     filter: progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)
     -ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)"
+    a,a:active,a:hover,a:focus,a:visited
+      color: white
+      text-decoration: underline
   .carousel-control-prev
     background-image: -webkit-linear-gradient(left,rgba(0,0,0,.5) 0,rgba(0,0,0,.0001) 100%);
     background-image: -o-linear-gradient(left,rgba(0,0,0,.5) 0,rgba(0,0,0,.0001) 100%);
