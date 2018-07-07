a037d5a5040faacff7f03e3488a0fd1180d79931
taichunmin <taichunmin@gmail.com>
2018-07-07 20:55:26 +0800

    新增神機妙算的規則

--- .vuepress/components/WizardScoreBoard.vue
+++ .vuepress/components/WizardScoreBoard.vue
@@ -4,7 +4,7 @@
   .card.mb-3
     .card-header
       ul.nav.nav-tabs.card-header-tabs
-        li.nav-item(v-for="tabName, tabKey in {setting: '設定', input: '記錄', result: '結果'}")
+        li.nav-item(v-for="tabName, tabKey in {setting: '設定', input: '記錄', scores: '得分', rule: '規則'}")
           a.nav-link(href="#", :class="{active: curTab == tabKey}", @click="curTab = tabKey") {{ tabName }}
     table.table.table-striped.table-borderless.mb-0(v-show="curTab == 'setting'")
       tbody
@@ -53,7 +53,7 @@
       button.btn.btn-sm.btn-outline-secondary.mr-1(type="button", :disabled="roundCur <= 0", @click="roundCur--") #[span.fa.fa-arrow-left] 上一輪
       button.btn.btn-sm.mr-1(type="button", :disabled="roundCur >= roundMax - 1", @click="roundCur++", :class="'btn-outline' + inputResultColor") #[span.fa.fa-arrow-right] 下一輪
       span.text-black-50.fs-dot8rem 點玩家名稱可以改名。
-    .table-responsive(v-if="curTab == 'result' && !!roundMax")
+    .table-responsive(v-if="curTab == 'scores' && !!roundMax")
       table.table.table-striped.table-borderless.table-sm.mb-0.text-center.text-nowrap.text-monospace#result-table
         thead
           tr
@@ -68,6 +68,29 @@
             td(v-for="player in players")
               template(v-if="roundsEnable[i-1]", v-for="round in [_.get(player, ['rounds', i-1])]")
                 span.round-score.pr-2.align-middle(v-for="score in [calculatePlayerRoundScore(round)]", :class="score > 0 ? 'text-success' : 'text-danger'") {{ score }}
+    .card-body.p-2(v-if="curTab == 'rule'")
+      img.mb-3.d-block.mx-auto.img-fluid(src="/images/boardgame-wizard.jpg")
+      p {{ players.length }} 人遊戲共有 {{ _.inRange(players.length, 3, 7) ? (60 / players.length) : 0 }} 局。 每局由一個人順時針輪流作為發牌玩家，第 1 局每人 1 張牌、第 2 局每人 2 張牌…依此類推。
+      p 沒發完的牌要翻出最上面的牌作為王牌，王牌的花色即為該局的「王牌色」。 若王牌為 Z 則由發牌玩家決定本局王牌色，若王牌為 N 或是最後一局全部發完時則以「無王牌色」進行。
+      p 每局開始時，由發牌玩家開始喊本局預測會吃幾墩，其他人順時針喊出預測會吃幾墩，需要用本記分板紀錄所有人所預測的墩數。
+      p 預測完後就進入出牌階段，每次由本局上一次吃墩的玩家先出一張牌，若是本局第一次出牌則由發牌者先出一張牌。
+      p 其他玩家必須順時針依序打出一張與先發者同花色的牌（Z 和 N 可作為任何花色），如果沒有同花色的牌，才能夠打出王牌色或其他花色的牌。 若先發者打出無花色的牌（Z 或 N），則由下一家出牌決定本次的花色。 若下一家又打出無花色的牌，則再由下一家出牌決定。
+      p 所有玩家都出一張牌以後，牌最大的玩家吃到這一墩（把這次所有人出的一張牌放於自己面前）。 當本局的手牌全部打完，本局結束。
+      p
+        | 每次所有玩家打出一張牌以後，透過以下的方式比較大小：
+        ol
+          li 最大的是 Z，若有多張 Z 則先出的人較大
+          li 第二大是王牌色，1 最小 13 最大
+          li 第三大是先發者的花色，1 最小 13 最大
+          li 第四大是非王牌色也非先發者的花色
+          li 最小的是 N，若有多張 N 則先出的人較大
+      p 每局結束時，需要將每個人所吃到的墩數紀錄於本記分板。
+      p
+        | 遊戲計分的方式為：
+        ul
+          li 若該局玩家預測正確，則分數為：墩數乘以 10 再加上額外的 20 分。
+          li 若該局玩家預測錯誤，則分數為：多吃或少吃的墩數乘以 -10。（沒有額外的 20 分）
+      p.mb-0 玩家每一局所獲得的分數加總即為總分，總分最多的人獲勝。
     .alert.alert-danger.mb-0(v-if="!roundMax") 神機妙算的人數限制為 3 人到 6 人。
 </template>
 
